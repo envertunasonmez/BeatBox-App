02d9b559a35a027b5e76ec56d613de954cd0291f
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-10-07 14:50:14 +0300

    create - api service

---
+++ lib/data/services/api_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/track_model.dart';
+
+class ApiService {
+  static const String popularUrl = "https://api.deezer.com/chart/0/tracks";
+
+  Future<List<Track>> fetchPopularTracks() async {
+    final http.Response response = await http.get(Uri.parse(popularUrl));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List tracksJson = data['data'];
+      return tracksJson.map((final json) => Track.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load tracks');
+    }
+  }
+
+  Future<List<Track>> searchTracks(final String query) async {
+    final http.Response response = await http.get(Uri.parse(
+        'https://api.deezer.com/search?q=${Uri.encodeComponent(query)}'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List tracksJson = data['data'];
+      return tracksJson.map((final json) => Track.fromJson(json)).toList();
+    } else {
+      throw Exception('Search failed');
+    }
+  }
+}
